0bd043eb60daf9cf6e8430aa2faf2a31aae6451c
jTCode2408 <jasmine.terry91@gmail.com>
2020-02-26 21:48:01 -0500

    file structure created. knex migration fo rtable file created

---
+++ data/migrations/20200226214602_create.js
@@ -0,0 +1,9 @@
+//user info table needs username, pass, dept(string, add index prop(?) for querying easier on this column)
+exports.up = function(knex) {
+  
+
+};
+
+exports.down = function(knex) {
+  
+};
